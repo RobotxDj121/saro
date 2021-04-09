b82a6605ebb79aa8a5ec31bea9dd29424526b086
saran833 <71981173+saran833@users.noreply.github.com>
2021-04-09 11:49:21 +0300

    Delete saran.js

--- commands/saran.js
+++
@@ -1,9 +0,0 @@
-module.exports.config = {
-    cooldown: '1',
-    usage: 'ping <member> <reason>',
-    description: "saran"
-}
-
-module.exports.run = async(client, message, args) => {
-    let msg = await message.channel.send('saran');
-}
